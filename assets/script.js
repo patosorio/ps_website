fe295b9c7cc52f28f62b859a7e96fd4a20e9f5df
pat <pat@MacBook-Air-de-pat.local>
2021-07-29 18:32:51 +0200

    First Commit

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,3 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
-    if (window.matchMedia('(max-width: 992px)').matches) {
-
-        var margin = $(".header").height();
-        console.log(margin);
-        $(".info-icon").css({"margin-top":margin+20});
-    }
-    else{
-
-        var gif = $("#gif");
-        gif.css({position: "absolute"});
-          
-        var mouseX = 0, mouseY = 0;
-        var xp = 0, yp = 0;
-
-        $(document).mousemove(function(e){
-            mouseX = e.pageX;
-            mouseY = e.pageY;
-        });
-
-        // Find Selector
-        
-        var loop = setInterval(function(){
-            xp += (mouseX - xp) / 10;
-            yp += (mouseY - yp) / 10;
-            gif.css({left:xp, top:yp});
-        }, 15);
-    }
 });
